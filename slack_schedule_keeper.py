b785bacaea8ef4515eaa5d636fe338125e27dd81
candale <candale.andrei@gmail.com>
2016-04-12 14:15:27 +0300

    First response on slack

--- slack_schedule_keeper.py
+++ slack_schedule_keeper.py
@@ -1,11 +1,9 @@
 import time
+import subprocess
 
 from slackclient import SlackClient
 
 import settings
-
-
-sc = SlackClient(settings.SLACK_TOKEN)
 
 
 def raise_if_not_ok(response):
@@ -23,6 +21,20 @@ def get_user_id_by_email(email):
 
     if not user:
         raise ValueError('No user for email {}'.format(email))
+
+    return user[0]['id']
+
+
+def get_user_id_by_name(name):
+    users_response = sc.api_call('users.list')
+    raise_if_not_ok(users_response)
+
+    users = users_response['members']
+
+    user = filter(lambda user: user['profile']['name'] == name, users)
+
+    if not user:
+        raise ValueError('No user for email {}'.format(name))
 
     return user[0]['id']
 
@@ -49,22 +61,42 @@ def get_chat_with_user(user_id):
 
 
 def process_message(message):
-    # TODO: Process messages with click!!
-    print message
+    if message['type'] != 'message':
+        return
+
+    return subprocess.check_output('gym_sub')
 
 
-def check_stuff():
-    pass
+BOT_NAME = 'schedule_keeper'
+BOT_ID = get_user_id_by_name(BOT_NAME)
+sc = SlackClient(settings.SLACK_TOKEN)
+
+
+def message_checks_out(message):
+    if not message:
+        return False
+
+    return all([
+        message['type'] == 'message',
+        message['user'] != BOT_ID
+    ])
 
 
 def run():
     if sc.rtm_connect():
         while True:
-            message = sc.rtm_read()
-            process_message(message)
-            check_stuff()
+            messages = sc.rtm_read()
+            message = messages[0] if messages else None
 
-            time.sleep(1)
+            if message_checks_out(message) is False:
+                time.sleep(1)
+                continue
+
+            channel = message['channel']
+            result = process_message(message)
+            sc.api_call(
+                'chat.postMessage', channel=channel, text=result, as_user=True)
+
     else:
         raise ValueError('Could not connect')
 
