f982da0877ba3f5b0195cff119f614c8baee8cd5
candale <candale.andrei@gmail.com>
2016-04-12 12:25:52 +0300

    Adapted for use with context manager. Multiple dates supported

--- cli.py
+++ cli.py
@@ -1,4 +1,5 @@
 import datetime
+from collections import namedtuple
 import re
 
 import click
@@ -6,30 +7,20 @@ import click
 from scheduler import CrossfitScheduler
 
 
-class DateParamType(click.ParamType):
+class DateTimeParamType(click.ParamType):
     name = 'date'
+    _fail_message = (
+        'datetime should be a string of the format DD-MM-YYYY-HH:MM')
 
-    def convert(self, value, param, ctx):
-        try:
-            date = datetime.datetime.strptime(value, '%d-%m-%Y').date()
-            return date
-        except ValueError:
-            self.fail('Date should be a string of the format DD-MM-YYYY')
-
-
-class TimeParamType(click.ParamType):
-    name = 'time'
-    _fail_message = 'Time should look like this HH:MM with 24 hour format'
-
-    def convert(self, value, param, ctx):
+    def _get_time(self, time_value):
         def check_bounds(value, lower, upper):
             if not lower <= value <= upper:
                 self.fail(self._fail_message)
 
-        if not re.match('\d{1,2}:\d{1,2}', value):
+        if not re.match('\d{1,2}:\d{1,2}', time_value):
             self.fail(self._fail_message)
 
-        hour_str, minute_str = value.split(':')
+        hour_str, minute_str = time_value.split(':')
         hour = int(hour_str)
         minute = int(minute_str)
 
@@ -37,6 +28,20 @@ class TimeParamType(click.ParamType):
         check_bounds(minute, 0, 60)
 
         return hour, minute
+
+    def convert(self, value, param, ctx):
+        DateTimeType = namedtuple('DateTimeType', ['date', 'time'])
+        try:
+            values = value.split('-')
+            time_str = values.pop()
+            date_str = '-'.join(values)
+
+            date = datetime.datetime.strptime(date_str, '%d-%m-%Y').date()
+            time = self._get_time(time_str)
+
+            return DateTimeType(date, time)
+        except ValueError:
+            self.fail(self._fail_message)
 
 
 class ClassParamType(click.ParamType):
@@ -66,51 +71,59 @@ def cli():
 
 
 @cli.group()
-@click.option('--email', type=click.STRING, required=True,
-              help='Email address for the registration')
-@click.option('--activity', type=ClassParamType(), required=True,
-              help='The activity you want to register for')
-@click.option('--date', type=DateParamType(), required=True,
-              help='The date for the registration')
-@click.option('--time', type=TimeParamType(), required=True,
-              help='The time of the registration')
-def schedule(email, activity, date, time):
+def gym_schedule():
     '''Manage registrations'''
     pass
 
 
-@schedule.command()
-def create(email, activity, date, time):
+@gym_schedule.command()
+@click.option('--email', type=click.STRING, required=True,
+              help='Email address for the registration')
+@click.option('--activity', type=ClassParamType(), required=True,
+              help='The activity you want to register for')
+@click.option('--date', type=DateTimeParamType(), required=True,
+              multiple=True, help='The dates separated by spaces')
+def create(email, activity, date):
     '''Register for a class'''
-    scheduler = CrossfitScheduler(email)
 
-    try:
-        if scheduler.schedule(activity, date, time):
-            click.echo('Scheduled you for {} on {} at {}:{}'.format(
-                activity, date, *time))
-        else:
-            click.echo('Could not schedule you for {} on {} at {}:{}'.format(
-                activity, date, *time))
-    except Exception, e:
-        raise click.ClickException('Failed with reason: {}'.format(e))
+    with CrossfitScheduler(email) as scheduler:
+        for date_time in date:
+            try:
+                if scheduler.schedule(
+                        activity, date_time.date, date_time.time):
+                    click.echo('Scheduled you for {} on {} at {}:{}'.format(
+                        activity, date_time.date, *date_time.time))
+                else:
+                    click.echo(
+                        'Could not schedule you for {} on {} at {}:{}'.format(
+                            activity, date_time.date, *date_time.time))
+            except Exception, e:
+                raise click.ClickException('Failed with reason: {}'.format(e))
 
 
-@schedule.command()
-def cancel(email, activity, date, time):
+@gym_schedule.command()
+@click.option('--email', type=click.STRING, required=True,
+              help='Email address for the registration')
+@click.option('--activity', type=ClassParamType(), required=True,
+              help='The activity you want to register for')
+@click.option('--date', type=DateTimeParamType(), required=True, nargs=10,
+              multiple=True, help='The date(s) for the registration')
+def cancel(email, activity, date):
     '''Cancel a registration'''
-    scheduler = CrossfitScheduler(email)
 
-    try:
-        if scheduler.cancel_schedule(activity, date, time):
-            click.echo('Canceled schedule for {} on {} at {}:{}'.format(
-                activity, date, *time))
-        else:
-            click.echo(
-                'Could not cancel schedule for {} on {} at {}:{}'.format(
-                    activity, date, *time))
-    except Exception, e:
-        raise click.ClickException('Failed with reason: {}'.format(e))
-
-
-if __name__ == '__main__':
-    cli()
+    with CrossfitScheduler(email) as scheduler:
+        for date_time in date:
+            try:
+                if scheduler.cancel_schedule(
+                        activity, date_time.date, date_time.time):
+                    click.echo(
+                        'Canceled schedule for {} on {} at {}:{}'.format(
+                            activity, date_time.date, *date_time.time))
+                else:
+                    click.echo(
+                        'Could not cancel schedule '
+                        'for {} on {} at {}:{}'.format(
+                            activity, date_time.date, *date_time.time)
+                    )
+            except Exception, e:
+                raise click.ClickException('Failed with reason: {}'.format(e))
